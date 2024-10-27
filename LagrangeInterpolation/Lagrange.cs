8c8595f059ffc14c011a40a01072ebd20ff6217c
Norbert Olesiak-Zbieg <n.olesiak@gmail.com>
2024-10-27 15:50:45 +0100

    Add project files.

---
+++ LagrangeInterpolation/Lagrange.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagrangeInterpolation
+{
+    public readonly struct InterpolationNode(double X, double Y)
+    {
+        public double X { get; } = X;
+        public double Y { get; } = Y;
+    }
+    public static class Lagrange
+    {
+        public static double LagrangeInterpolation(InterpolationNode[] nodes, double xp)
+        {
+            int n = nodes.Length;
+            double result = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                double term = nodes[i].Y;
+                for (int j = 0; j < n; j++)
+                {
+                    if (j != i)
+                    {
+                        term *= (xp - nodes[j].X) / (nodes[i].X - nodes[j].X);
+                    }
+                }
+                result += term;
+            }
+            Console.WriteLine($"W({xp}) = {result}");
+            return result;
+        }
+    }
+}
---
+++ LagrangeInterpolation/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using LagrangeInterpolation;
+
+PritnStudentInfo();
+
+
+InterpolationNode[] points = [
+            new(1.0, 2.0),
+            new(2.0, 3.0),
+            new(3.0, 5.0),
+            new(4.0, 8.0),
+            new(5.0, 13.0),
+            new(6.0, 21.0),
+            new(7.0, 34.0),
+            new(8.0, 51.0),
+            new(9.0, 85.0),
+            new(10.0, 136.0)
+        ];
+
+InterpolationNode[] points3 = [
+            new(1.0, 2.0),
+            new(2.0, 3.0),
+            new(3.0, 5.0)
+        ];
+
+
+
+var xp = 2.5; 
+
+_ = Lagrange.LagrangeInterpolation(points, xp);
+_ = Lagrange.LagrangeInterpolation(points3, xp);
+
+FinishProgram();
+
+
+static void FinishProgram()
+{
+    Console.WriteLine();
+    Console.WriteLine("===");
+    Console.WriteLine("Program completed.");
+    Console.WriteLine("Press Enter to close.");
+    Console.WriteLine("===");
+    Console.ReadLine();
+    Environment.Exit(0);
+}
+
+static void PritnStudentInfo()
+{
+    Console.WriteLine("=== ===");
+    Console.WriteLine("=== Lagrange Interpolation ===");
+    Console.WriteLine("=== Norbert Olesiak-Zbieg ===");
+    Console.WriteLine("=== Informatyka II Stopnia ===");
+    Console.WriteLine("=== nr albumu 41237 ===");
+    Console.WriteLine("=== ===");
+    Console.WriteLine();
+}
+
